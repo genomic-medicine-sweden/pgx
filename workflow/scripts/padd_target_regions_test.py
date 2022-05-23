78bbe7f9304e617c81e88f7483ada4f1ecc3d623
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-05-23 14:22:58 +0200

    fix: remove wrong test framework tests

--- workflow/scripts/padd_target_regions_test.py
+++ workflow/scripts/padd_target_regions_test.py
@@ -38,29 +38,3 @@ class TestPaddTargetRegions(unittest.TestCase):
                 "failed test '{}': expected {}, got {}".format(
                     case.name, case.expected, actual),
             )
-
-
-import pytest
-import padd_target_regions as ptr
-
-
-def test_switch_coordinates_no_switch():
-    coordinates = ["chr1", 1, 2, "ID1"]
-    assert ptr.switch_coordinates_if_reversed(coordinates) == coordinates
-
-
-def test_switch_coordinates_switch_needed():
-    coordinates = ["chr1", 2, 1, "ID1"]
-    assert ptr.switch_coordinates_if_reversed(coordinates) == ["chr1", 1, 2, "ID1"]
-
-@pytest.fixture(scope="session")
-def bed_file(tmp_path_factory):
-    fn = tmp_path_factory.mktemp("data") / "test.bed"
-    content = "chr1\t200\t300\tID1"
-    p.write_text(content)
-    return fn
-
-
-def test_add_padding(bed_file):
-    bed_fn = bed_file()
-    padded_bed = add_padding(bed_fn, 100)
