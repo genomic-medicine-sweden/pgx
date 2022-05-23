1fd18ac01051d9c285024dde5c1df321871c8e40
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-05-23 13:49:42 +0200

    test: check that switching of coordinates works

--- workflow/scripts/padd_target_regions_test.py
+++ workflow/scripts/padd_target_regions_test.py
@@ -1,13 +1,44 @@
-# vim: syntax=python tabstop=4 expandtab
-# coding: utf-8
-"""
-Test functions used for padding bed files
-"""
+import unittest
+from dataclasses import dataclass
+from pathlib import Path
+import pandas as pd
+import tempfile
+import padd_target_regions as ptr
+from typing import List
 
-__author__ = "Lauri Mesilaakso"
-__copyright__ = "Copyright 2022, Lauri Mesilaakso"
-__email__ = "lauri.mesilaakso@regionostergotland.se"
-__license__ = "GPL-3"
+
+class TestPaddTargetRegions(unittest.TestCase):
+
+    def test_switch_coordinates_if_reversed(self):
+
+        @dataclass
+        class TestCase:
+            name: str
+            input: List
+            expected: List
+
+        testcases = [
+            TestCase(
+                name="Switch coordinates successfully",
+                input=["chr1", 2, 1, "ID1"],
+                expected=["chr1", 1, 2, "ID1"],
+            ),
+            TestCase(
+                name="Don't switch coordinates when it's not necessary",
+                input=["chr1", 1, 2, "ID1"],
+                expected=["chr1", 1, 2, "ID1"],
+            ),
+        ]
+
+        for case in testcases:
+            actual = ptr.switch_coordinates_if_reversed(case.input)
+            self.assertEqual(
+                case.expected,
+                actual,
+                "failed test '{}': expected {}, got {}".format(
+                    case.name, case.expected, actual),
+            )
+
 
 import pytest
 import padd_target_regions as ptr
