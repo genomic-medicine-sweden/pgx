7cdb75c5957f39dd81cd7f57f65d8535d1d89431
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-05-23 13:48:13 +0200

    feat: add padd_target_regions rule

--- workflow/rules/padd_target_regions.smk
+++ workflow/rules/padd_target_regions.smk
@@ -21,15 +21,30 @@ rule padd_target_regions:
             "pgx/padd_target_regions/{sample}_{type}.output.benchmark.tsv",
             config.get("padd_target_regions", {}).get("benchmark_repeats", 1),
         )
-    threads: config.get("padd_target_regions", {}).get("threads", config["default_resources"]["threads"])
+    threads:
+        config.get("padd_target_regions", {}).get(
+            "threads", config["default_resources"]["threads"]
+        )
     resources:
-        threads=config.get("padd_target_regions", {}).get("threads", config["default_resources"]["threads"]),
-        time=config.get("padd_target_regions", {}).get("time", config["default_resources"]["time"]),
-        mem_mb=config.get("padd_target_regions", {}).get("mem_mb", config["default_resources"]["mem_mb"]),
-        mem_per_cpu=config.get("padd_target_regions", {}).get("mem_per_cpu", config["default_resources"]["mem_per_cpu"]),
-        partition=config.get("padd_target_regions", {}).get("partition", config["default_resources"]["partition"]),
+        threads=config.get("padd_target_regions", {}).get(
+            "threads", config["default_resources"]["threads"]
+        ),
+        time=config.get("padd_target_regions", {}).get(
+            "time", config["default_resources"]["time"]
+        ),
+        mem_mb=config.get("padd_target_regions", {}).get(
+            "mem_mb", config["default_resources"]["mem_mb"]
+        ),
+        mem_per_cpu=config.get("padd_target_regions", {}).get(
+            "mem_per_cpu", config["default_resources"]["mem_per_cpu"]
+        ),
+        partition=config.get("padd_target_regions", {}).get(
+            "partition", config["default_resources"]["partition"]
+        ),
     container:
-        config.get("padd_target_regions", {}).get("container", config["default_container"])
+        config.get("padd_target_regions", {}).get(
+            "container", config["default_container"]
+        )
     conda:
         "../envs/padd_target_regions.yaml"
     message:
--- workflow/scripts/padd_target_regions.py
+++ workflow/scripts/padd_target_regions.py
@@ -11,8 +11,10 @@ __email__ = "lauri.mesilaakso@regionostergotland.se"
 __license__ = "GPL-3"
 
 from pathlib import Path
+import logging
 from typing import List
 import pandas as pd
+import csv
 
 
 def switch_coordinates_if_reversed(coordinates: List) -> List:
@@ -26,31 +28,44 @@ def switch_coordinates_if_reversed(coordinates: List) -> List:
     """
     chrom, start, end, region_id = coordinates
     if start > end:
+        logging.info(f"Original start coordinate: {str(start)}"
+                     f", end coordinate: {str(end)}")
         end, start = start, end
     return [chrom, start, end, region_id]
 
 
-def add_padding(target_bed: Path, padding: int) -> pd.DataFrame:
+def read_df(input_bed_file: Path) -> pd.DataFrame:
+    """Read an input 4 column bed file into a pandas dataframe
+
+    Args:
+        input_bed_file (Path): Path to input 4 column bed file
+
+    Returns:
+        pd.DataFrame: Parsed pandas dataframe containing the bed file data
+    """
+    return pd.read_csv(input_bed_file,
+                       sep="\t",
+                       names=["CHROM", "START", "END", "ID"],
+                       dtype={
+                           "START": int,
+                           "END": int
+                       })
+
+
+def add_padding(target_bed_df: pd.DataFrame, padding: int) -> pd.DataFrame:
     """
     Add padding to target start and end coordinates
 
     Args:
-        target_bed (Path): Path to input bed file
+        target_bed_df (pd.DataFrame): Bed file as a pandas dataframe
         padding (int): The number bases of padding to add to start
                         and end coordinates
 
     Returns:
         pd.DataFrame: Pandas dataframe with padded start and end coordinates
     """
-    targets: pd.DataFrame = pd.read_csv(target_bed,
-                                        sep="\t",
-                                        names=["CHROM", "START", "END", "ID"],
-                                        dtype={
-                                            "START": int,
-                                            "END": int
-                                        })
     rows: list = []
-    for _, row in targets.iterrows():
+    for _, row in target_bed_df.iterrows():
         chrom, start, end, region_id = switch_coordinates_if_reversed(row[0:4])
         start -= padding
         end += padding
@@ -58,32 +73,32 @@ def add_padding(target_bed: Path, padding: int) -> pd.DataFrame:
     return pd.DataFrame(rows, columns=["CHROM", "START", "END", "ID"])
 
 
-def write_output(input_table: pd.DataFrame, output_f: Path) -> None:
+def write_output(input_table: pd.DataFrame, out_file: Path) -> None:
     """
     Write pandas df table into a bed file format
     Args:
         input_table (pd.DataFrame): Pandas data frame with columns:
                                     ["CHROM", "START", "END", "ID"]
-        output_f (Path): Path to output file
+        out_file (Path): Path to output file
     """
 
-    with open(output_f, "w", encoding="utf-8") as f_out:
+    with open(out_file, "w", encoding="utf-8") as f_out:
+        tsv_writer = csv.writer(f_out, delimiter="\t")
         for _, row in input_table.iterrows():
             chrom, start, end, region_id = row[0:4]
-            f_out.write(f"chr{chrom}\t{start}\t{end}\t{region_id}\n")
+            tsv_writer.writerow([chrom, start, end, region_id])
 
 
-def main() -> None:
-    """
-    Run the main program
-    """
+if __name__ == '__main__':
     target_bed = snakemake.input.target_regions
     output_file_name = snakemake.output.padded_target_regions
     padding = snakemake.params.padding
 
-    padded_coordinates = add_padding(Path(target_bed), int(padding))
+    logging.basicConfig(level=logging.INFO, filename=snakemake.log[0])
+
+    logging.info(f"Read {target_bed}")
+    bed_df = read_df(Path(target_bed))
+    padded_coordinates = add_padding(bed_df, int(padding))
+
     write_output(padded_coordinates, Path(output_file_name))
-
-
-if __name__ == '__main__':
-    main()
+    logging.info(f"Successfully written padded bed file {output_file_name}")
